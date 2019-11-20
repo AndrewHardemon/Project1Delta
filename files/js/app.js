017759c81ce268266d7894e552a45e2290ffc970
Andrew Hardemon <aeroatlas@ymail.com>
2019-11-19 21:27:56 -0700

    added special character replacements, poster, description, and undisable for checkbox

--- files/js/app.js
+++ files/js/app.js
@@ -8,18 +8,24 @@ var apiKey = "92f5c8ff853ffea4d1fed070c2f2d729"
 var guidebox = "58812660af2896e3f7dea2cddda186e473191fba"
 var tries = 0;
 var finalResult = "";
+var descArray = [];
 
 //Click Event
 $(".form-check-input").on("click", function(event){
   type = $(this).val();
   console.log(type);
-  $(".form-check-input").attr("disabled", "disabled");
+  $(".form-check-input").attr("disabled", "true");
 });
 
 //Submitting the data
 $("#submit").on("click", function(event){
   event.preventDefault();
   console.log("this works");
+
+  //Undo Disable and Checked for type
+  $(".form-check-input").prop('disabled', false)
+  $(".form-check-input").prop('checked', false)
+
   //Gets genre variable
   genre = $("#genre").val().trim();
   console.log(genre);
@@ -36,9 +42,12 @@ $("#submit").on("click", function(event){
   console.log(r3);
   console.log(r4);
 
+  //Exclude adult content
+  var adult = "&include_adult=false";
+
   sources = $("#sources").val().trim();
   // var page; var randomPage;
-  var queryURL = `https://api.themoviedb.org/3/discover/${type}?with_genres=${genre}&with_runtime.gte=${r3}&with_runtime.lte=${r4}&api_key=${apiKey}&language=en-US&page=1`
+  var queryURL = `https://api.themoviedb.org/3/discover/${type}?with_genres=${genre}&with_runtime.gte=${r3}&with_runtime.lte=${r4}&api_key=${apiKey+adult}&language=en-US&page=1`
   console.log(queryURL);
 
   function totalAjax(){
@@ -90,9 +99,44 @@ $("#submit").on("click", function(event){
       }
         console.log(outputName);
 
+      //Get rid of special characters
+      function cleanUpSpecialChars(str){
+        return str
+          .replace(/[ÀÁÂÃÄÅÆ]/g,"A")
+          .replace(/[àáâãäåæ]/g,"a")
+          .replace(/[Ç]/g,"C")
+          .replace(/[ç]/g,"c")
+          .replace(/[ÈÉÊË]/g,"E")
+          .replace(/[èéêë]/g,"e")
+          .replace(/[ÌÍÎÏ]/g,"I")
+          .replace(/[ìíîï]/g,"i")
+          .replace(/[Ñ]/g,"N")
+          .replace(/[ñ]/g,"n")
+          .replace(/[ÒÓÔÕÖØ]/g,"O")
+          .replace(/[òóôõöø]/g,"o")
+          .replace(/[Š]/g,"S")
+          .replace(/[š]/g,"s")
+          .replace(/[ß]/g,"ss")
+          .replace(/[ÚÛÜÙ]/g,"U")
+          .replace(/[ùúûü]/g,"u")
+          .replace(/[ÝŸ]/g,"Y")
+          .replace(/[ýÿ]/g,"y")
+          .replace(/[Ž]/g,"Z")
+          .replace(/[ž]/g,"Z")
+          .replace(/[^\x00-\x7F]+/g,'') //non ascii
+          //.replace(/[^a-z0-9]/gi,''); // final clean up
+      }
+      cleanUpSpecialChars(outputName);
+
       //Replace spaces with -
       outputName = outputName.replace(/\ /g, '-')
       console.log(outputName)
+      
+      //Get Description for the Movie/Show
+      console.log(queryURL);
+      console.log(res.results[total].overview)
+      var desc = res.results[total].overview
+      descArray.push(desc);
 
       //Gets QueryURL for Guidebox for movie or tv
       var queryURL2;
@@ -106,6 +150,7 @@ $("#submit").on("click", function(event){
         queryURL2 = `http://api-public.guidebox.com/v2/search?api_key=${guidebox}&type=show&field=title&query=${outputName}`
       }
       console.log(queryURL2)
+      console.log(`http://api-public.guidebox.com/v2/sources?api_key=${guidebox}&filter=movie&type=movie&field=title&query=${outputName}`)
 
       //Guidebox Ajax
       $.ajax({
@@ -120,6 +165,7 @@ $("#submit").on("click", function(event){
           //adds tries and resets the totalAjax function
           tries++
           console.log(`Its been ${tries} tries`)
+          descArray = [];
           totalAjax();
         } else if (res.total_results > 1){
           var total2 = Math.floor((Math.random() * res.total_results));
@@ -128,13 +174,48 @@ $("#submit").on("click", function(event){
           //Get random title out of the list
           finalResult = res.results[total2].title;
           console.log(finalResult)
+
+          var artworkTV = res.results[total2].poster_240x342
+          var artworkMovie = res.results[total2].artwork_304x171
+          // var artworkTV = res.results[total2].poster_120x171
+          // var artworkMovie = res.results[total2].artwork_208x117
+          console.log(artworkTV)
+          console.log(artworkMovie)
           //$("#outputs").html(finalResult)
         } else {
           finalResult = res.results[0].title;
           console.log(finalResult)
+          var artworkTV = res.results[0].poster_240x342
+          var artworkMovie = res.results[0].artwork_304x171
+          console.log(artworkTV)
+          console.log(artworkMovie)
           //$("#outputs").html(finalResult)
         }
+
+        //Output the Title
         $("#outputs").text(finalResult)
+        var artwork; //empty var
+        //Check if its movie or tv
+        if(artworkTV == undefined){
+          artwork = artworkMovie
+        } else if (artworkMovie == undefined){
+          artwork = artworkTV
+        } else {
+          console.log("error with poster")
+        }
+        console.log(artwork)
+
+        //Output the Artwork
+        var poster = $("<img>");
+        poster.attr("src", artwork);
+        $("#outputs").append(poster);
+
+        //Output the Description
+        var desc = $("<p>");
+        desc.append(descArray[0])
+        console.log(descArray[0]);
+        $("#outputs").append(desc);
+
       });
     });
   });
